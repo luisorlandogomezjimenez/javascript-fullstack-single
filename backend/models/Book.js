d4305753b4b0c0d5a7386b968fc8c8b504123db4
Orlando <luisorlandogomezjimenez@gmail.com>
2021-09-24 11:59:28 -0500

    routes, models, database

---
+++ backend/models/Book.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  author: {
+    type: String,
+    require: true,
+  },
+  isbn: {
+    type: String,
+    require: true,
+  },
+  imagePath: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = model("Book", BookSchema);
