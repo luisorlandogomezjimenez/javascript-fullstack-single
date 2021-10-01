08d3a004d9c32bef5b6d95b5fccb49329222bda9
Orlando <luisorlandogomezjimenez@gmail.com>
2021-09-30 20:09:09 -0500

    UI.js

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 
 const router = Router();
+const { unlink } = require("fs-extra");
+const path = require("path");
 
 const Book = require("../models/Book");
 
@@ -11,14 +13,15 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   const { title, author, isbn } = req.body;
-  const newBook = new Book({ title, author, isbn });
+  const imagePath = "/uploads/" + req.file.filename;
+  const newBook = new Book({ title, author, isbn, imagePath });
   await newBook.save();
   res.json({ message: "Book Saved" });
 });
 
 router.delete("/:id", async (req, res) => {
-  await Book.findByIdAndDelete(req.params.id);
-
+  const book = await Book.findByIdAndDelete(req.params.id);
+  unlink(path.resolve("./backend/public" + book.imagePath));
   res.json({ message: "book deleted" });
 });
 
